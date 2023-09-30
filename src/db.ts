38452ac93ce367406c92a694e76e072737d1f5bf
Tais Batista <tbc1301@gmail.com>
2023-09-30 16:56:03 -0300

    add upload

---
+++ src/db.ts
@@ -0,0 +1,42 @@
+export const db = [
+  {
+    img: 'assets/ac-cover.jpg',
+    label: 'NEW',
+    type: 'Digital PS5',
+    price: '209,99',
+  },
+
+  {
+    img: 'assets/bt-1.jpg',
+    label: 'EXCLUSIVE',
+    type: 'Digital PS4',
+    price: '129,99',
+  },
+
+  {
+    img: 'assets/bt-4.jpg',
+    label: 'DIGITAL',
+    type: 'Digital PS5',
+    price: '189,99',
+  },
+
+  {
+    img: 'assets/bt-5.jpg',
+    label: 'EXCLUSIVE',
+    type: 'Digital PS4, PS5',
+    price: '209,99',
+  },
+
+  {
+    img: 'assets/bt-bad-company-2.jpg',
+    label: 'NEW',
+    type: 'Digital PS5',
+    price: '229,99',
+  },
+  {
+    img: 'assets/bt-hardline.jpg',
+    label: 'SUCCESS ',
+    type: 'Digital PS4, PS5',
+    price: '209,99',
+  },
+];
